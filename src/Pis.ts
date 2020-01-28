0e5c6a6471396972b0461cbeaf5395e7cfc4ed34
anjan <anjan.som@gmail.com>
2020-01-28 15:47:39 +0100

    Added service to get session_id before redirecting PSU to Connect

--- src/Pis.ts
+++ src/Pis.ts
@@ -21,8 +21,8 @@ export class PIS {
     this.axiosInstance = this._getAxiosInstance(config.env);
   }
 
-    /**
-   * Initiate
+  /**
+   * Prepare
    *
    * @param {string} accessToken
    * @param {object} payload
